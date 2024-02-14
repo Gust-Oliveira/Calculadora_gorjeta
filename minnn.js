dd6258aca79a5ce3ff4086123b246dcc268649f5
Gustavo de Oliveira <gustavo.oliveira201@hotmail.com>
2024-02-14 14:28:09 -0300

    deletando arquivo desnecessário

--- minnn.js
+++
@@ -1,9 +0,0 @@
-function teste(ar){
-    console.log(Math.max(...ar), 'maior número entre:', ar.toString())   
-    console.log(Math.min(...ar), 'menor número entre:', ar.toString())
-}
-
-
-let arrTeste = [90,3,6,5,4,9,8,7]
-
-teste(arrTeste)
